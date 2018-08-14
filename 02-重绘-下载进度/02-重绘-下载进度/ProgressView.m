b85f05af7e40b109ff0be5974568cd48b9898e89
juningwei <juningwei0908@sina.com>
2018-08-14 23:13:50 +0800

    02

---
+++ 02-重绘-下载进度/02-重绘-下载进度/ProgressView.m
@@ -0,0 +1,21 @@
+//
+//  ProgressView.m
+//  02-重绘-下载进度
+//
+//  Created by 鞠凝玮 on 2018/8/14.
+//  Copyright © 2018年 Ju. All rights reserved.
+//
+
+#import "ProgressView.h"
+
+@implementation ProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ 02-重绘-下载进度/02-重绘-下载进度/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02-重绘-下载进度
+//
+//  Created by 鞠凝玮 on 2018/8/14.
+//  Copyright © 2018年 Ju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
